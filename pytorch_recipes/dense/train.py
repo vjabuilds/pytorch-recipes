c3c7c84105f73ee6b4f03068f62f06213ae9dc3d
vjabuilds <jovan.vjestica@outlook.com>
2022-11-16 11:07:35 +0100

    Removed dataset instantiation from Trainer

--- pytorch_recipes/dense/train.py
+++ pytorch_recipes/dense/train.py
@@ -1,14 +1,13 @@
+import torch
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from .pandas_dataset import PandasDataset
 from .pandas_dataset import Formats
-from .dense import DenseNet
 
 
 class Trainer():
     def train(self, dataset, network, epochs):
-        dataset = PandasDataset('./fashion-mnist_train.csv', Formats.CSV, 'label', 10)
         loader = DataLoader(dataset, batch_size=64, shuffle=True)
 
         crit = nn.CrossEntropyLoss()
@@ -27,3 +26,4 @@ class Trainer():
                 if i % 200 == 199:
                     print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 200:.3f}')
                     running_loss = 0.0
+        torch.save(network.state_dict(), 'weights.pth')
